ef7d1b69f3b63d70664bdec56873a060795cb257
rivels <53620138+rivels@users.noreply.github.com>
2020-10-28 20:49:47 +0900

    melon_scraping

--- scraping.py
+++ scraping.py
@@ -1,5 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.project
 
 
 #melon 봄
@@ -7,68 +11,136 @@ headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/
 data_Mspring = requests.get('https://www.melon.com/dj/djfinder/djfinder_inform.htm?djSearchType=T&djSearchKeyword=%EB%B4%84',headers=headers)
 soup_Mspring = BeautifulSoup(data_Mspring.text, 'html.parser')
 
-#멜론 봄 플레이리스트 이름&이미지가 들어갈 리스트 정의 추후 딕셔너리형태로 바꿔야 될 수도 있을
-Mspring_title=Mspring_img=[]
-
-'''
-for문이 크롤링 하나당 하나씩 생기는게 아니라,
-하나의 for문에 여러가지 항목(제목,이미지,곡수,좋아요수)을 크롤링할 수 있도록,또 동시
-계절별 db에 dictionary 형식으로
-{
- 'melon_spring_title':M_playlist1.strip()
- 'melon_spring_img':M_playlist11
- .....
-} 와 같은 형식으로 입력해야할듯
-
-플레이리스트 제목, DJ, 태그, 이미지
-'''
+Mspring_title=[]
+Mspring_img=[]
+Mspring_tag=[]
+Mspring_num=[]
 
 Mlist_spring_title=soup_Mspring.select('#djPlylstList > div > ul > li > div.entry > div.info')
 Mlist_spring_img=soup_Mspring.select('#djPlylstList > div > ul > li > div.thumb > a')
+Mlist_spring_tag=soup_Mspring.select('#djPlylstList > div > ul > li > div.entry > div.tag_list > a.tag_item')
+Mlist_spring_num=soup_Mspring.select('#djPlylstList > div > ul > li > div.entry > div.meta > span')
 for title in Mlist_spring_title:
     M_playlist1=title.select_one('a.ellipsis.album_name').text
     Mspring_title.append(M_playlist1.strip())
 for img in Mlist_spring_img:
     M_playlist11=img.select_one('img')['src']
     Mspring_img.append(M_playlist11)
-print(Mspring_title,Mspring_img)
-#melon 여름
+for tag in Mlist_spring_tag:
+    Mspring_tag.append(tag.text)
+for num in Mlist_spring_num:
+    Mspring_num.append(num.text)
+for d in range(0,len(Mlist_spring_title)):
+    M_sprint_doc ={
+        'title' : Mspring_title[d],
+        'img' : Mspring_img[d],
+        'tag_1' : Mspring_tag[d],
+        'tag_2' : Mspring_tag[d+1],
+        'num' : Mspring_num[d]
+    }
+    db.melon_spring.insert_one(M_sprint_doc)
 
+
+#melon 여름
 
 data_Msummer = requests.get('https://www.melon.com/dj/djfinder/djfinder_inform.htm?djSearchType=T&djSearchKeyword=%ED%95%B4%EB%B3%80',headers=headers)
 soup_Msummer = BeautifulSoup(data_Msummer.text, 'html.parser')
 
-melon_playlist_summer=[]
+Msummer_title=[]
+Msummer_img=[]
+Msummer_tag=[]
+Msummer_num=[]
 
-Msummer_title=soup_Msummer.select('#djPlylstList > div > ul > li > div.entry > div.info')
+Mlist_summer_title=soup_Msummer.select('#djPlylstList > div > ul > li > div.entry > div.info')
+Mlist_summer_img=soup_Msummer.select('#djPlylstList > div > ul > li > div.thumb > a')
+Mlist_summer_tag=soup_Msummer.select('#djPlylstList > div > ul > li > div.entry > div.tag_list > a.tag_item')
+Mlist_summer_num=soup_Msummer.select('#djPlylstList > div > ul > li > div.entry > div.meta > span')
+for title in Mlist_summer_title:
+    M_playlist1=title.select_one('a.ellipsis.album_name').text
+    Msummer_title.append(M_playlist1.strip())
+for img in Mlist_summer_img:
+    M_playlist11=img.select_one('img')['src']
+    Msummer_img.append(M_playlist11)
+for tag in Mlist_summer_tag:
+    Msummer_tag.append(tag.text)
+for num in Mlist_summer_num:
+    Msummer_num.append(num.text)
+for d in range(0,len(Mlist_summer_title)):
+    M_summer_doc ={
+        'title' : Msummer_title[d],
+        'img' : Msummer_img[d],
+        'tag_1' : Msummer_tag[d],
+        'tag_2' : Msummer_tag[d+1],
+        'num' : Msummer_num[d]
+    }
+    db.melon_summer.insert_one(M_summer_doc)
 
-for summertitle in Msummer_title:
-    M_playlist2 = summertitle.select_one('a.ellipsis.album_name').text
-    melon_playlist_summer.append(M_playlist2.strip())
-print(melon_playlist_summer)
 
 #melon 가을
+
 data_Mfall = requests.get('https://www.melon.com/dj/djfinder/djfinder_inform.htm?djSearchType=T&djSearchKeyword=%23%EA%B0%80%EC%9D%84',headers=headers)
 soup_Mfall = BeautifulSoup(data_Mfall.text, 'html.parser')
 
-melon_playlist_fall=[]
+Mfall_title=[]
+Mfall_img=[]
+Mfall_tag=[]
+Mfall_num=[]
 
 Mlist_fall_title=soup_Mfall.select('#djPlylstList > div > ul > li > div.entry > div.info')
+Mlist_fall_img=soup_Mfall.select('#djPlylstList > div > ul > li > div.thumb > a')
+Mlist_fall_tag=soup_Mfall.select('#djPlylstList > div > ul > li > div.entry > div.tag_list > a.tag_item')
+Mlist_fall_num=soup_Mfall.select('#djPlylstList > div > ul > li > div.entry > div.meta > span')
+for title in Mlist_fall_title:
+    M_playlist1=title.select_one('a.ellipsis.album_name').text
+    Mfall_title.append(M_playlist1.strip())
+for img in Mlist_fall_img:
+    M_playlist11=img.select_one('img')['src']
+    Mfall_img.append(M_playlist11)
+for tag in Mlist_fall_tag:
+    Mfall_tag.append(tag.text)
+for num in Mlist_fall_num:
+    Mfall_num.append(num.text)
+for d in range(0,len(Mlist_fall_title)):
+    M_fall_doc ={
+        'title' : Mfall_title[d],
+        'img' : Mfall_img[d],
+        'tag_1' : Mfall_tag[d],
+        'tag_2' : Mfall_tag[d+1],
+        'num' : Mfall_num[d]
+    }
+    db.melon_fall.insert_one(M_fall_doc)
 
-for k in Mlist_fall_title:
-    M_playlist3 = k.select_one('a.ellipsis.album_name').text
-    melon_playlist_fall.append(M_playlist3.strip())
-print(melon_playlist_fall)
 
 #melon 겨울
+
 data_Mwinter = requests.get('https://www.melon.com/dj/djfinder/djfinder_inform.htm?djSearchType=T&djSearchKeyword=%23%EA%B2%A8%EC%9A%B8',headers=headers)
 soup_Mwinter = BeautifulSoup(data_Mwinter.text, 'html.parser')
 
-melon_playlist_winter=[]
+Mwinter_title=[]
+Mwinter_img=[]
+Mwinter_tag=[]
+Mwinter_num=[]
 
 Mlist_winter_title=soup_Mwinter.select('#djPlylstList > div > ul > li > div.entry > div.info')
-
-for l in Mlist_winter_title:
-    M_playlist4 = l.select_one('a.ellipsis.album_name').text
-    melon_playlist_winter.append(M_playlist4.strip())
-print(melon_playlist_winter)
+Mlist_winter_img=soup_Mwinter.select('#djPlylstList > div > ul > li > div.thumb > a')
+Mlist_winter_tag=soup_Mwinter.select('#djPlylstList > div > ul > li > div.entry > div.tag_list > a.tag_item')
+Mlist_winter_num=soup_Mwinter.select('#djPlylstList > div > ul > li > div.entry > div.meta > span')
+for title in Mlist_winter_title:
+    M_playlist1=title.select_one('a.ellipsis.album_name').text
+    Mwinter_title.append(M_playlist1.strip())
+for img in Mlist_winter_img:
+    M_playlist11=img.select_one('img')['src']
+    Mwinter_img.append(M_playlist11)
+for tag in Mlist_winter_tag:
+    Mwinter_tag.append(tag.text)
+for num in Mlist_winter_num:
+    Mwinter_num.append(num.text)
+for d in range(0,len(Mlist_winter_title)):
+    M_winter_doc ={
+        'title' : Mwinter_title[d],
+        'img' : Mwinter_img[d],
+        'tag_1' : Mwinter_tag[d],
+        'tag_2' : Mwinter_tag[d+1],
+        'num' : Mwinter_num[d]
+    }
+    db.melon_winter.insert_one(M_winter_doc)
--- scrapingPrac01.py
+++ scrapingPrac01.py
@@ -7,14 +7,12 @@ headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/
 data_Mspring = requests.get('https://www.melon.com/dj/djfinder/djfinder_inform.htm?djSearchType=T&djSearchKeyword=%EB%B4%84',headers=headers)
 soup_Mspring = BeautifulSoup(data_Mspring.text, 'html.parser')
 
-#djPlylstList > div > ul > li:nth-child(1) // 멜론 플레이리스트 li 까지의 selector
-Mlist=soup_Mspring.select('#djPlylstList > div > ul > li')
-# print(Mlist)
+A=[]
+Mlist_spring_num=soup_Mspring.select('#djPlylstList > div > ul > li > div.entry > div.meta > span')
 
-#djPlylstList > div > ul > li:nth-child(1) > div.entry > div.info > a.ellipsis.album_name // 플레이리스트 title
-#djPlylstList > div > ul > li:nth-child(1) > div.entry > div.info > a.ellipsis.artist_name // DJ명도 동일하게 안됨..
-#djPlylstList > div > ul > li:nth-child(1) > div.entry > div.tag_list > a:nth-child(1) > span
+Total=soup_Mspring.select('#djPlylstList > div > ul > li > div.entry')
 
-for M in Mlist:
-    M_tag=M.select_one('div.entry > div.tag_list > a > span').text
-    print(M_tag)
+for _ in Total:
+    num=_.select_one('div.meta > span')
+    A.append(num.text)
+print(A)
